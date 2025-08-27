c8eb2109adb62e552c7200afa4ccad2a7c8bef50
Braddiiee <makhayawilliams.info@gmail.com>
2025-08-27 20:38:17 -0300

    feat(competition): enhance CompetitionsList component

--- frontend/src/pages/Competitions/CompetitionsList.tsx
+++ frontend/src/pages/Competitions/CompetitionsList.tsx
@@ -1,5 +1,39 @@
-export default function CompetitionsList() {
+import matchesData from "../../data/matches";
+import type { Match } from "../../types";
+
+const matches: Match[] = matchesData as Match[];
+
+const CompetitionsList: React.FC = () => {
+
+    // Get unique competition titles
+    const competitions = Array.from ( new Set (matches.map( (m) => m.competition_title) ));
+
     return (
-        <div>hi</div>
-    )
-}
+        <div className="space-y-8">
+            {competitions.map((competition) => (
+                <div key={competition}>
+                    <h2 className="text-xl font-bold">{competition}</h2>
+                    <div className="space-y-4">
+                    {matches
+                    .filter((match) => match.competition_title === competitions)
+                    .map((match) => (
+                        <MatchCard
+                        key={match.match_id}
+                        homeTeam={match.home_team}
+                        awayTeam={match.away_team}
+                        matchScore={match.match_score}
+                        matchStatus={match.match_status}
+                        liveMinutes={match.live_duration_minutes}
+                        matchTime={match.match_time_utc}
+                        matchDate={match.match_date}
+                        />
+                    ))}
+                </div>
+
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default CompetitionsList;
