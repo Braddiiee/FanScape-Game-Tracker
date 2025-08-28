8eba838490fc6e6714284752ac6e8e051b256510
Braddiiee <makhayawilliams.info@gmail.com>
2025-08-28 20:08:37 -0300

    refactor(index): remove unnecessary props from index.tsx for cleaner component usage

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,19 +1,14 @@
-import { mockRecommendedMatches, mockFavoriteTeamMatches } from "./MockData";
 import RecommendedMatches from "./RecommendedMatches";
 import FavoriteTeamMatches from "./FavoriteTeamMatches.tsx";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-50 p-6">
-      <div className="max-w-6xl mx-auto py-10">
-        
-        <h1 className="text-4xl font-extrabold text-center text-gray-900 mb-12">
-          FanScape Home
-        </h1>
+    <div className="min-h-screen p-6">
+      <div className="min-w-6xl mx-auto py-10">
 
-        <RecommendedMatches matches={mockRecommendedMatches} />
+        <RecommendedMatches />
 
-        <FavoriteTeamMatches matches={mockFavoriteTeamMatches} />
+        <FavoriteTeamMatches  />
       </div>
     </div>
   );
