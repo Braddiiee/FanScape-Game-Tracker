03786100b466374e483c30cad9e69a12ca7118c9
Braddiiee <makhayawilliams.info@gmail.com>
2025-08-28 14:45:22 -0300

    feat(app): integrate Session and Bcrypt, enable automatic db.create_all() in app factory

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_session import Session
 from flask_cors import CORS # Cross-Origin Resource Sharing
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 def create_app():
     app = Flask(__name__)
@@ -13,9 +16,15 @@ def create_app():
     CORS(app) # Let frontend (React talk to the backend) 
 
     db.init_app(app)
+    bcrypt.init_app(app)
+    Session(app)
+
 
     from .routes import api
     app.register_blueprint(api, url_prefix="/api")
+
+    with app.app_context():
+        db.create_all()
 
     return app
 
