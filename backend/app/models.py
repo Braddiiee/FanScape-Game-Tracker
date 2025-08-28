3e7628478fecf9d75d2e743502f681f9880f2c02
Braddiiee <makhayawilliams.info@gmail.com>
2025-08-28 15:41:02 -0300

    feat(models): align User, Game, and Team models with frontend data structures

--- backend/app/models.py
+++ backend/app/models.py
@@ -8,26 +8,49 @@ class User(db.Model):
     username = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    birth_day = db.Column(db.Integer, nullable=False)
-    birth_month = db.Column(db.Integer, nullable=False)
-    birth_year = db.Column(db.Integer, nullable=False)
+    birth_date = db.Column(db.Date, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # The to_dict() method converts the Python object into a dictionary, which can then easily be sent as JSON via an API response.
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "birthDate": self.birth_date.isoformat() if self.birth_date else None,
+            "email": self.email
+        }
+
 
 # Games model
 class Games(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    game = db.Column(db.String(100), unique=True, nullable=False)
+    match_title = db.Column(db.String(100),  nullable=False)
+    home_team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+    away_team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
     content =  db.Column(db.String(100), nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.DateTime)
+    home_team = db.relationship('Teams', foreign_keys=[home_team_id])
+    away_team = db.relationship('Teams', foreign_keys=[away_team_id])
     sport = db.Column(db.String(100), nullable=False)
     league = db.Column(db.String(100))
-    team_1 = db.Column(db.String(100))
-    team_2 = db.Column(db.String(100))
+
+    # The to_dict() method converts the Python object into a dictionary, which can then easily be sent as JSON via an API response.
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "match_title": self.match_title,
+            "home_team": {"team_id": self.home_team.id, "team_name": self.home_team.team_name},
+            "away_team": {"team_id": self.away_team.id, "team_name": self.away_team.team_name},
+            "date": self.date.isoformat()
+        }
+
+
 
 # Teams Model 
 class Teams(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     team_name = db.Column(db.String(100), unique=True, nullable=False)
+    logo_url = db.Column(db.String(255), nullable=True)
 
 # Many-to-many relationships
 game_saves = db.Table(
