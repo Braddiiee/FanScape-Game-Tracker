dcdba4771053b87e1aa8460ae796d0bed9cff1c5
Braddiiee <makhayawilliams.info@gmail.com>
2025-08-27 22:10:49 -0300

    chore(context): split FiltersContext into its own file for context onl

--- frontend/src/context/FiltersContext.tsx
+++ frontend/src/context/FiltersContext.tsx
@@ -1,5 +1,4 @@
-import { createContext, useContext, useState } from "react";
-import type { ReactNode } from "react";
+import { createContext } from "react";
 
 // Define the shape of our filter state
 type Filters = {
@@ -10,25 +9,6 @@ type Filters = {
 }
 
 // Create context
-const FiltersContext = createContext<Filters | undefined>(undefined);
+export const FiltersContext = createContext<Filters | undefined>(undefined);
 
-// Provider component wraps the app 
-export function FiltersProvider({ children }: {children: ReactNode}) {
-    const [sport, setSport] = useState<string | null>(null);
-    const [date, setDate] = useState<string | null>(null);
 
-    return (
-        <FiltersContext.Provider value={{ sport, date, setSport, setDate }}>
-            {children}
-        </FiltersContext.Provider>
-    );
-}
-
-// Custom hoook for consuming context
-function useFilters() {
-    const context = useContext(FiltersContext);
-    if (!context) throw new Error("useFilters must be used inside FiltersProvider");
-    return context;
-}
-
-export default useFilters;
